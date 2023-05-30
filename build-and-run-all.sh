7851a4d78b1684ce9d45df8b2e1508211c3c2062
kajacx <kajacx@gmail.com>
2023-05-30 21:03:18 +0200

    Add a build script

---
+++ build-and-run-all.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/sh
+
+echo "Clearing plugins cache" && \
+rm -rf plugins && mkdir plugins && \
+\
+echo "Building Rust plugin" && \
+cd plugin-rust && cargo build --target wasm32-unknown-unknown && cd .. && \
+cp plugin-rust/target/wasm32-unknown-unknown/debug/plugin_rust.wasm plugins/plugin_rust.wasm && \
+\
+echo "Runntig Rust wasmer host" && \
+cd runtime-rust-wasmer && cargo run && cd .. && \
+\
+echo "All done"

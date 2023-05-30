029e4c639d06ca2a47c84a2533dd22ec0107ba00
kajacx <kajacx@gmail.com>
2023-05-30 21:12:08 +0200

    Load module file bytes in rust wasmer runtime

--- runtime-rust-wasmer/src/main.rs
+++ runtime-rust-wasmer/src/main.rs
@@ -1,1 +1,22 @@
-fn main() {}
+fn main() {
+    println!("{}", load_module_files().len());
+}
+
+fn load_module_files() -> Vec<(String, Vec<u8>)> {
+    let entries = std::fs::read_dir("../plugins").expect("should read plugins directory");
+
+    let mut results = vec![];
+    for entry in entries {
+        let path = entry.expect("should find directory entry").path();
+        let name = path
+            .file_name()
+            .expect("should get filename")
+            .to_str()
+            .expect("should convert filename to str")
+            .to_string();
+        let content = std::fs::read(path).expect("should read plugin bytes from file");
+        results.push((name, content));
+    }
+
+    results
+}
